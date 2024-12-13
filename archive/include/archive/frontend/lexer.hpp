9adc6d214be0c1bae044fbd1fcdd471f2cfff930
michael-re <102764781+michael-re@users.noreply.github.com>
2024-12-13 11:40:08 -0800

    feat: add on demand lexer

---
+++ archive/include/archive/frontend/lexer.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef ARCHIVE_FRONTEND_LEXER_HPP
+#define ARCHIVE_FRONTEND_LEXER_HPP
+
+#include <string>
+#include <optional>
+
+#include "archive/frontend/scanner.hpp"
+#include "archive/frontend/token.hpp"
+
+namespace archive::frontend
+{
+    class Lexer
+    {
+    public:
+        explicit Lexer(std::string filename, std::string source);
+
+        auto lex()     -> Lexer&;
+        auto advance() -> const Token&;
+        auto consume() -> const Token&;
+
+        [[nodiscard]] auto peek_prev() const -> const Token&;
+        [[nodiscard]] auto peek_curr() const -> const Token&;
+        [[nodiscard]] auto peek_next() const -> const Token&;
+
+    private:
+        Scanner              m_scanner;
+        std::optional<Token> m_previous;
+        std::optional<Token> m_current;
+        std::optional<Token> m_next;
+    };
+} // namespace archive::frontend
+
+#endif // ARCHIVE_FRONTEND_LEXER_HPP
---
+++ archive/source/archive/frontend/lexer.cpp
@@ -0,0 +1,53 @@
+#include "archive/frontend/lexer.hpp"
+#include "archive/common/assert.hpp"
+
+using namespace archive;
+using namespace archive::frontend;
+
+Lexer::Lexer(std::string filename, std::string source)
+    : m_scanner (std::move(filename), std::move(source)),
+      m_previous(std::nullopt),
+      m_current (m_scanner.scan()),
+      m_next    (m_scanner.scan())
+{
+}
+
+auto Lexer::lex() -> Lexer&
+{
+    if (m_current != Token::Type::EndOfFile)
+    {
+        m_previous = std::move(m_current);
+        m_current  = std::move(m_next);
+        m_next     = m_scanner.scan();
+    }
+
+    return *this;
+}
+
+auto Lexer::advance() -> const Token&
+{
+    return lex().peek_curr();
+}
+
+auto Lexer::consume() -> const Token&
+{
+    return lex().peek_prev();
+}
+
+auto Lexer::peek_prev() const -> const Token&
+{
+    ASSERT(m_previous, "trying to access previous token when it does not exist");
+    return *m_previous;
+}
+
+auto Lexer::peek_curr()  const -> const Token&
+{
+    ASSERT(m_current, "trying to access current token when it does not exist");
+    return *m_current;
+}
+
+auto Lexer::peek_next() const -> const Token&
+{
+    ASSERT(m_next, "trying to access next token when it does not exist");
+    return *m_next;
+}
